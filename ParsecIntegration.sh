4d17bb9ddb8704fa5b00187bb734296612e70a22
Kozova1 <30871100+Kozova1@users.noreply.github.com>
2017-08-09 21:40:47 -0700

    Update ParsecIntegration.sh

--- ParsecIntegration.sh
+++ ParsecIntegration.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 variable answer3
-variable answer2
-variable answer1
-variable answer
-echo "This script integrate Parsec with RetroPie. Parsec will appear within the Ports section in RetroPie. You have to have RetroPie and ParsecInstalled!!!"
+echo "This script intete Parsec with RetroPie. Parsec will appear within the Ports section in RetroPie. You have to have RetroPie and ParsecInstalled!!!"
 echo "Made by Kozova1"
 echo "What is YOUR server id? it's located on the server console tab."
 read answer3
 cd /home/pi/RetroPie/roms/ports/
 echo -n 'parsec server_id=' >Parsec.sh
 echo $answer3 > Parsec.sh
-echo  "What is your Parsec Email adress?"
-read answer
-echo  "What is your Parsec password? This script won't store it"
-read answer1
-echo  "Do you want to save your Email & password? (y/n)"
-read answer2
 parsec
-echo $answer
-echo $answer1
-echo $answer2
