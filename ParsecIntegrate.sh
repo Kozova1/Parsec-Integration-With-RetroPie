35cb808f7e7bd3f4b1aea12755e66a9c963532eb
Kozova1 <30871100+Kozova1@users.noreply.github.com>
2017-08-16 17:30:18 +0300

    Update ParsecIntegrate.sh

--- ParsecIntegrate.sh
+++ ParsecIntegrate.sh
@@ -1,8 +1,7 @@
 echo "This script will integrate Parsec with RetroPie it will appear within RetroPie in the ports section"
 echo "Made by Kozova1"
 read -p "Type your servers ID its on the server console tab"
-cd 
-cd RetroPie/roms/ports
+cd /home/pi/RetroPie/roms/ports
 sudo echo -n "parsec server_id=" > Parsec.sh
 sudo echo $REPLY > Parsec.sh
 parsec
