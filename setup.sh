3156b6c9c51c51467fb5636c7bdaeef61273705c
Kozova1 <30871100+Kozova1@users.noreply.github.com>
2017-08-30 12:59:36 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 wget https://s3.amazonaws.com/parsec-build/package/parsec-rpi.deb
 sudo dpkg -i parsec-rpi.deb
 sudo chmod +x Parsec.sh
-./Parsec.sh
+sudo tr -d '\r' Parsec.sh ParsecFixed.sh
+./ParsecFixed.sh
