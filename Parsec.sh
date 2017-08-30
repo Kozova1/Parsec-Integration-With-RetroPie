6fba9fcec1f60e1fe7a6949d082ae809c8c61ad0
Kozova1 <30871100+Kozova1@users.noreply.github.com>
2017-08-30 13:07:41 +0300

    Delete Parsec.sh

--- Parsec.sh
+++
@@ -1,7 +0,0 @@
-echo "This script will integrate Parsec with RetroPie it will appear within RetroPie in the ports section"
- echo "Made by Kozova1"
- echo "Type your servers ID its on the server console tab"
-read REPLY
-cd ~pi/RetroPie/roms/ports/
-sudo echo -n "parsec server_id=$REPLY">Parsec.sh
-parsec
