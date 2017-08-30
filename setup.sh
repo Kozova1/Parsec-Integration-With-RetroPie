290d7d6ac708de0a331812ceeca5d5a88cc2b237
Kozova1 <30871100+Kozova1@users.noreply.github.com>
2017-08-30 18:48:24 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@
 wget https://s3.amazonaws.com/parsec-build/package/parsec-rpi.deb
 sudo dpkg -i parsec-rpi.deb
 sudo chmod +x Parsec.sh
+sudo cp es_systems.cfg /etc/emulationstation/es_system.cfg
+sudo mkdir -p ~pi/RetroPie/roms/ports
 ./Parsec.sh
