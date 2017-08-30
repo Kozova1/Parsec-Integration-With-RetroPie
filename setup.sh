7f49a2a8ecc7513956ddcdf9878c60d70efeb6e1
Kozova1 <30871100+Kozova1@users.noreply.github.com>
2017-08-30 13:05:05 +0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget https://s3.amazonaws.com/parsec-build/package/parsec-rpi.deb
-sudo dpkg -i parsec-rpi.deb
-sudo chmod +x Parsec.sh
-sudo tr -d '\r' Parsec.sh ParsecFixed.sh
-./ParsecFixed.sh
