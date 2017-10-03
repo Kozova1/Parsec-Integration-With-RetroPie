2875c096a9ed173cdbf4e384ee42827ca987411a
Kozova1 <30871100+Kozova1@users.noreply.github.com>
2017-10-03 19:54:33 +0300

    Update Parsec.sh

--- Parsec.sh
+++ Parsec.sh
@@ -2,7 +2,8 @@ echo "This script will integrate Parsec with RetroPie it will appear within Retr
 echo "Made by Kozova1"
 echo "Type your servers ID its on the server console tab"
 read REPLY
-cd ~pi/RetroPie/roms/ports/
+mkdir -p ~pi/RetroPie/roms/parsec
+cd ~pi/RetroPie/roms/parsec/
 sudo echo -n "parsec server_id=$REPLY">Parsec.sh\
 sudo modprobe -r xpad
 sudo apt-get install xboxdrv
