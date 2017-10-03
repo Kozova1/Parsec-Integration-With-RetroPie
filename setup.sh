ddb6898fed5442d0b04052900e32d8c8cd0491fa
Kozova1 <30871100+Kozova1@users.noreply.github.com>
2017-10-03 22:30:45 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 wget https://s3.amazonaws.com/parsec-build/package/parsec-rpi.deb
 sudo dpkg -i parsec-rpi.deb
 sudo chmod +x Parsec.sh
-sudo mv --force /etc/emulationstation/es_systems.cfg /etc/emulationstation/es_systems.cfg.backup
-sudo mv --force es_systems.cfg /etc/emulationstation/es_systems.cfg
+sudo mv --force /opt/retropie/configs/all/emulationstation/es_systems.cfg /opt/retropie/configs/all/emulationstation/es_systems.cfg.backup
+sudo mv --force es_systems.cfg /opt/retropie/configs/all/emulationstation/es_systems.cfg
 sudo mkdir -p ~pi/RetroPie/roms/ports
 ./Parsec.sh
