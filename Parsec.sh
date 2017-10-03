e626004ce6ee8bd876de6a8a251a02249ca6a2a4
Kozova1 <30871100+Kozova1@users.noreply.github.com>
2017-10-03 20:39:21 +0300

    Update Parsec.sh

--- Parsec.sh
+++ Parsec.sh
@@ -2,6 +2,7 @@ echo "This script will integrate Parsec with RetroPie it will appear within Retr
 echo "Made by Kozova1"
 echo "Type your servers ID its on the server console tab"
 read REPLY
+mv --force themes /etc/emulationstation/themes
 mkdir -p ~pi/RetroPie/roms/parsec
 cd ~pi/RetroPie/roms/parsec/
 sudo echo -n "parsec server_id=$REPLY">Parsec.sh\
