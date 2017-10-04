9fc437c5e691c5c918ec80d42a709ab631e70c2b
Kozova1 <30871100+Kozova1@users.noreply.github.com>
2017-10-04 09:03:26 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 wget https://s3.amazonaws.com/parsec-build/package/parsec-rpi.deb
 sudo dpkg -i parsec-rpi.deb
 sudo chmod +x Parsec.sh
+sudo rm -rf /etc/emulationstation/themes/nes-mini/parsec
+sudo rm -rf /etc/emulationstation/themes/carbon/parsec
 sudo mv --force /opt/retropie/configs/all/emulationstation/es_systems.cfg /opt/retropie/configs/all/emulationstation/es_systems.cfg.backup
 sudo mv --force es_systems.cfg /opt/retropie/configs/all/emulationstation/es_systems.cfg
 sudo mkdir -p ~pi/RetroPie/roms/ports
