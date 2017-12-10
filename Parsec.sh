bf9858b2f38c48df61391fef47a9233992635489
Kozova1 <30871100+Kozova1@users.noreply.github.com>
2017-12-10 21:27:09 +0200

    Update Parsec.sh

--- Parsec.sh
+++ Parsec.sh
@@ -3,7 +3,7 @@ echo "Made by Kozova1"
 echo "Type your servers ID its on the server console tab"
 read REPLY
 sudo cp -Rf ~pi/Parsec-Integration-With-RetroPie/carbon/ /etc/emulationstation/carbon/
-sudo cp -Rf ~pi/Parsec-Integration-With-RetroPie/carbon/ /opt/retropie/configs/all/emulationstation/themes/carbon/parsec
+sudo cp -Rf ~pi/Parsec-Integration-With-RetroPie/carbon/ /opt/retropie/configs/all/emulationstation/themes/carbon
 mkdir -p ~pi/RetroPie/roms/parsec
 cd ~pi/RetroPie/roms/parsec/
 sudo echo -n "parsec server_id=$REPLY">Parsec.sh
