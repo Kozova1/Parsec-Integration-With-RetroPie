59eee4de31df00c2be997021af4a28a27624f44b
Kozova1 <30871100+Kozova1@users.noreply.github.com>
2018-03-02 21:18:20 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Made by Kozova1"
 wget https://s3.amazonaws.com/parsec-build/package/parsec-rpi.deb
 sudo dpkg -i parsec-rpi.deb
 sudo chmod +x Parsec.sh
@@ -6,5 +7,18 @@ sudo rm -rf /etc/emulationstation/themes/carbon/parsec
 sudo rm -rf /opt/retropie/configs/all/emulationstation/themes/carbon/parsec
 sudo mv --force /opt/retropie/configs/all/emulationstation/es_systems.cfg /opt/retropie/configs/all/emulationstation/es_systems.cfg.backup
 sudo mv --force es_systems.cfg /opt/retropie/configs/all/emulationstation/es_systems.cfg
+echo "Parsec entry added to EmulationStation"
 sudo mkdir -p ~pi/RetroPie/roms/ports
-./Parsec.sh
+echo "Type your servers ID its on the server console tab"
+read REPLY
+sudo cp -Rf ~pi/Parsec-Integration-With-RetroPie/carbon/ /etc/emulationstation/carbon/
+sudo cp -Rf ~pi/Parsec-Integration-With-RetroPie/carbon/ /opt/retropie/configs/all/emulationstation/themes/carbon
+echo "Carbon theme altered to fit Parsec"
+mkdir -p ~pi/RetroPie/roms/parsec
+cd ~pi/RetroPie/roms/parsec/
+sudo echo -n "parsec server_id=$REPLY">Parsec.sh
+echo "ROM File written"
+sudo modprobe -r xpad
+sudo apt-get install xboxdrv
+echo "Unneccessary driver removed, Installed better one. Starting parsec for the 1st time..."
+parsec
