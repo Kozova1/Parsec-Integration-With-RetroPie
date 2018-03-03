b5b3e33a50f14288ffd933d9a363b15a39d514cc
Kozova1 <30871100+Kozova1@users.noreply.github.com>
2018-03-03 13:56:07 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 echo "Made by Kozova1"
-wget https://s3.amazonaws.com/parsec-build/package/parsec-rpi.deb
-sudo dpkg -i parsec-rpi.deb
-sudo chmod +x Parsec.sh
-sudo rm -rf /etc/emulationstation/themes/carbon/parsec
-sudo rm -rf /opt/retropie/configs/all/emulationstation/themes/carbon/parsec
-sudo mv --force /opt/retropie/configs/all/emulationstation/es_systems.cfg /opt/retropie/configs/all/emulationstation/es_systems.cfg.backup
+if[ $1 != noparsec]
+then
+  if[ $2 != noparsec]
+  then
+    if[ $3 != noparsec]
+    then
+      wget https://s3.amazonaws.com/parsec-build/package/parsec-rpi.deb
+      sudo dpkg -i parsec-rpi.deb
+      sudo chmod +x Parsec.sh
+    fi
+  fi
+fi
+if [ $1 != notheme]
+then
+  if[ $2 != notheme]
+  then
+    if [$3 != notheme]
+      sudo rm -rf /etc/emulationstation/themes/carbon/parsec
+      sudo rm -rf /opt/retropie/configs/all/emulationstation/themes/carbon/parsec
+    fi
+  fi
+fi
+sudo cp --force /opt/retropie/configs/all/emulationstation/es_systems.cfg /opt/retropie/configs/all/emulationstation/es_systems.cfg.backup
 sed -i '$e cat parsec.cfg' /opt/retropie/configs/all/emulationstation/es_systems.cfg
 echo "Parsec entry added to EmulationStation"
 sudo mkdir -p ~pi/RetroPie/roms/ports
@@ -20,8 +37,13 @@ sudo echo -n "parsec server_id=$REPLY">Parsec.sh
 echo "ROM File written"
 if [ $1 != nodrv]
 then
-  sudo modprobe -r xpad
-  sudo apt-get install xboxdrv
+  if[ $2 != nodrv]
+  then
+    if[ $3 != nodrv]
+    then
+    sudo apt-get install xboxdrv
+    fi
+  fi
 fi
 echo "Unneccessary driver removed, Installed better one. Starting parsec for the 1st time..."
 parsec
