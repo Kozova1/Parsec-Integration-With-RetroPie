4e8f27f45cf576bb65e834ae9c36081e0931fbe5
Kozova1 <30871100+Kozova1@users.noreply.github.com>
2018-03-04 08:31:25 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,13 +9,15 @@ echo "Made by Kozova1"
       sudo rm -rf /etc/emulationstation/themes/carbon/parsec
       sudo rm -rf ~pi/.emulationstation/themes/carbon/parsec
 sudo cp --force ~pi/.emulationstation/es_systems.cfg ~pi/.emulationstation/es_systems.cfg.backup
-sed -i '$e cat parsec.cfg' /opt/retropie/configs/all/emulationstation/es_systems.cfg
+sudo cp --force /etc/emulationstation/es_systems.cfg /etc/emulationstation/es_systems.cfg.backup
+sed -i '$e cat parsec.cfg' ~pi/.emulationstation/es_systems.cfg
+sed -i '$e cat parsec.cfg' /etc/emulationstation/es_systems.cfg
 echo "Parsec entry added to EmulationStation"
 sudo mkdir -p ~pi/RetroPie/roms/ports
 echo "Type your servers ID its on the server console tab"
 read REPLY
-sudo cp -Rf ~pi/Parsec-Integration-With-RetroPie/carbon/ /etc/emulationstation/carbon/
-sudo cp -Rf ~pi/Parsec-Integration-With-RetroPie/carbon/ /opt/retropie/configs/all/emulationstation/themes/carbon
+sudo cp -Rf ~pi/Parsec-Integration-With-RetroPie/carbon/ /etc/emulationstation/themes/carbon/
+sudo cp -Rf ~pi/Parsec-Integration-With-RetroPie/carbon/ ~pi/.emulationstation/themes/carbon
 echo "Carbon theme altered to fit Parsec"
 mkdir -p ~pi/RetroPie/roms/parsec
 cd ~pi/RetroPie/roms/parsec/
