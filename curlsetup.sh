da19484751a07d7627d49edcd828f086ec36a0a3
Kozova1 <30871100+Kozova1@users.noreply.github.com>
2018-07-08 21:59:24 +0300

    Update curlsetup.sh

--- curlsetup.sh
+++ curlsetup.sh
@@ -2,4 +2,4 @@ sudo apt install git
 cd ~pi/
 git clone https://github.com/Kozova1/Parsec-Integration-With-RetroPie.git
 cd Parsec-Integration-With-RetroPie
-sudo bash setup.sh
+sudo bash setup.sh -nodrv
